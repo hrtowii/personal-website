f74299ee0118dcb4d92da93835da583b80a471c3
hrtowii <68852354+hrtowii@users.noreply.github.com>
2024-08-22 08:40:03 +0800

    im so done bro i hate this

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,12 +2,12 @@ import React, {useEffect, useState} from 'react';
 import {useLanyard, type Data} from 'use-lanyard';
 import './Sidebar.css'
 import MusicCard from './MusicCard';
-
+// import GitHubCalendar from 'react-github-calendar';
 export default function Sidebar() {
     const DISCORD_ID = '413331641109446656';
 	const {data} = useLanyard(DISCORD_ID);
     const [avatarLink, setAvatarLink] = useState("");
-    console.log(data?.discord_user.avatar)
+    // console.log(data?.discord_user.avatar)
     useEffect(() => {
         setAvatarLink(`https://cdn.discordapp.com/avatars/${DISCORD_ID}/${data?.discord_user.avatar ?? '60dd1a376bf9e675e002ee5acbdab313'}.png?size=512`)
     }, [data])
@@ -26,8 +26,8 @@ export default function Sidebar() {
                 </div>
             </div>
             <p>If you want to chat, feel free to reach out to me on <a className='twitter' href='https://twitter.com/htrowii'><span className='twitter'>Twitter</span></a> and <a className='discord' href='https://discord.com/users/413331641109446656'><span className='discord'>Discord</span></a>. You can also <a className='email-me' href="mailto:me@hrtowii.dev"><span className='email-me'>email me at me@hrtowii.dev</span></a></p>
-            {/* <GitHubCalendar username="hrtowii" /> */}
             </div>
+            {/* <GitHubCalendar username="hrtowii" /> */}
             <div className="sidebar-bottom">
                 <MusicCard/>
             </div>
--- src/components/SkillsCard.tsx
+++ src/components/SkillsCard.tsx
@@ -5,28 +5,54 @@ function SkillsCard() {
     <>
       <h2 style={{ margin: 0 }}>Skills</h2>
       <div className='skills'>
+
+        <div id="skill3" className="skill-box">
+          <strong><p>App Dev</p></strong>
+          <div className='skillflex'>
+            <div className='skillleft'>
+              <div className="skill"><i className="fa-brands fa-react"></i><p>React Native</p></div>
+              <div className="skill"><i className="fa-brands fa-flutter"></i><p>Flutter</p></div>
+            </div>
+            <div className='skillright'>
+              <div className="skill"><i className="fa-brands fa-swift"></i><p>SwiftUI</p></div>
+              <div className="skill"><img className='fa-brands' src='https://developer.android.com/static/events/dev-challenge/images/dev-challenge_jetpack-compose-asset.svg'></img><p>Jetpack Compose</p></div>
+            </div>
+          </div>
+        </div>
+        <div id="skill5" className="skill-box">
+          <strong><p>Web Dev</p></strong>
+          <div className='skillflex'>
+            <div className='skillleft'>
+              <div className="skill"><i className="fa-brands fa-react"></i><p>React</p></div>
+              <div className="skill"><img src='/nextjs.svg' className='fa-brands'></img><p>NextJS</p></div>
+            </div>
+            <div className='skillright'>
+              <div className="skill"><img src='https://angular.dev/assets/images/press-kit/angular_icon_gradient.gif' className='fa-brands'></img><p>Angular</p></div>
+              <div className="skill"><img src='https://www.svgrepo.com/show/354210/prisma.svg' className='fa-brands prisma'></img><p>Prisma</p></div>
+            </div>
+          </div>
+        </div>
+        <div id="skill4" className="skill-box">
+          <strong><p>Languages</p></strong>
+          <div className='skillflex'>
+            <div className='skillleft'>
+              <div className="skill"><i className="fa-brands fa-js"></i><p>JS/TS</p></div>
+              <div className="skill"><i className="fa-brands fa-python"></i><p>Python</p></div>
+            </div>
+            <div className='skillright'>
+              <div className="skill"><i className="fa-brands fa-js"></i><p>JS/TS</p></div>
+              <div className="skill"><i className="fa-brands fa-python"></i><p>Python</p></div>
+            </div>
+          </div>
+        </div>
+
         <div id="skill1" className="skill-box">
           <strong><p>VCS</p></strong>
-          <div className="skill"><i className="fa-brands fa-github"></i><p>Git, GitHub</p></div>
+          <div className="skill"><i className="fa-brands fa-github"></i><p>Git(Hub)</p></div>
         </div>
         <div id="skill2" className="skill-box">
           <strong><p>Design</p></strong>
           <div className="skill"><i className="fa-brands fa-figma"></i><p>Figma</p></div>
-        </div>
-        <div id="skill3" className="skill-box">
-          <strong><p>App Dev</p></strong>
-            <div className="skill"><i className="fa-brands fa-flutter"></i><p>Flutter</p></div>
-            <div className="skill"><i className="fa-brands fa-react"></i><p>React Native</p></div>
-            <div className="skill"><i className="fa-brands fa-swift"></i><p>SwiftUI</p></div>
-        </div>
-        <div id="skill4" className="skill-box">
-          <strong><p>Languages</p></strong>
-          <div className="skill"><i className="fa-brands fa-js"></i><p>JS/TS</p></div>
-          <div className="skill"><i className="fa-brands fa-python"></i><p>Python</p></div>
-
-        </div>
-        <div id="skill5" className="skill-box">
-          
         </div>
       </div>
     </>
