a8a2a48b98b0673722a1b01d3f4b58f784cd60da
hrtowii <68852354+hrtowii@users.noreply.github.com>
2024-09-30 00:37:09 +0800

    feat: better blogposts

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import './BlogCard.css'
-function BlogCard() {
-  return (
-    <div className='blog-card'>
-      <h2>Blog</h2>
+import React from 'react';
+import './BlogCard.css';
+import FancyHyperlink from './FancyHyperlink';
 
-    </div>
-  )
+interface BlogPost {
+  title: string;
+  date: string;
+  slug: string;
 }
 
-export default BlogCard
+interface BlogCardProps {
+  blogPosts: BlogPost[];
+}
+
+function BlogCard({ blogPosts }: BlogCardProps) {
+  return (
+    <div className='blog-card'>
+      <div className='blog-card-title'>
+        <h2>Blog</h2>
+        <FancyHyperlink href="/blog" content='view all' />
+      </div>
+      <div className='blog-entries'>
+        {blogPosts.map((post, index) => (
+          <div key={index} className='blog-entry'>
+            <div className='blog-entry-part1'>
+              <h3>{post.title}</h3>
+              <p>{post.date}</p>
+            </div>
+            <div className='blog-entry-part2'>
+              <FancyHyperlink href={`/blog/${post.slug}`} content="Read More" />
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default BlogCard;
