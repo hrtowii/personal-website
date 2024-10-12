e63a9b9ff3a93dfcb1e462615e48461ff3c9714a
hrtowii <68852354+hrtowii@users.noreply.github.com>
2024-10-13 02:10:49 +0800

    colors, image,

--- src/components/ProjectItem.tsx
+++ src/components/ProjectItem.tsx
@@ -7,9 +7,10 @@ interface ProjectItemProps {
   repo: string;
   link: string;
   description: string;
+  images?: string[];
 }
 
-const ProjectItem: React.FC<ProjectItemProps> = ({ id, title, repo, link, description }) => {
+const ProjectItem: React.FC<ProjectItemProps> = ({ id, title, repo, link, description, images }) => {
   const [stars, setStars] = useState<number | string>('Loading');
 
   useEffect(() => {
@@ -33,29 +34,38 @@ const ProjectItem: React.FC<ProjectItemProps> = ({ id, title, repo, link, descri
     <div id={id} className="item">
       <div className="item-wrapper">
         <div className="item-content">
-        <div className="item-title">
-          <h4>{title}</h4>
-          <div className="stars">
-            <svg 
-              aria-hidden="true" 
-              height="16" 
-              viewBox="0 0 16 16" 
-              version="1.1" 
-              width="16" 
-              data-view-component="true" 
-              className="octicon octicon-star-fill starred-button-icon d-inline-block mr-2"
-            >
-              <path 
-                fill="#e5b84e" 
-                d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Z"
-              ></path>
-            </svg>
-            <div id={`projectStars_${repo.replace('/', '_')}`}>
-              <p>{stars}</p>
+          <div className="item-title">
+            <h4>{title}</h4>
+            <div className="stars">
+              <svg
+                aria-hidden="true"
+                height="16"
+                viewBox="0 0 16 16"
+                version="1.1"
+                width="16"
+                data-view-component="true"
+                className="octicon octicon-star-fill starred-button-icon d-inline-block mr-2"
+              >
+                <path
+                  fill="#e5b84e"
+                  d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Z"
+                ></path>
+              </svg>
+              <div id={`projectStars_${repo.replace('/', '_')}`}>
+                <p>{stars}</p>
+              </div>
             </div>
           </div>
-        </div>
-          <p>{description}</p>
+          <div className="item-content-again">
+            <p>{description}</p>
+            {images && images.length > 0 && (
+                <div className="carousel-container">
+                  {images.map((url, index) => (
+                    <img key={index} src={url} alt={`project-${id}-image-${index}`} className="carousel-image" />
+                  ))}
+                </div>
+            )}
+          </div>
         </div>
         <FancyHyperlink content="Link to content" href={link} />
       </div>
