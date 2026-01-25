86219be03d67fec0ce95866e22c8a4806cb7d9d6
hrtowii <68852354+hrtowii@users.noreply.github.com>
2026-01-26 00:53:58 +0800

    pls

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,21 @@
-import { defineConfig } from "astro/config";
+// import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import nekoweb from "@indiefellas/astro-adapter-nekoweb";
-import { loadEnv } from "vite";
+// import nekoweb from "@indiefellas/astro-adapter-nekoweb";
+// import { loadEnv } from "vite";
+//
+// const env = loadEnv(process.env.NODE_ENV, process.cwd(), "");
+// // https://astro.build/config
+// export default defineConfig({
+//   integrations: [react()],
+//   adapter: nekoweb({
+//     apiKey: env.NEKOWEB_API_KEY,
+//     // cookie: 'your nekoweb cookie for recently updated support (optional)',
+//     folder: "dist",
+//   }),
+// });
+import { defineConfig } from 'astro/config';
 
-const env = loadEnv(process.env.NODE_ENV, process.cwd(), "");
-// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-  adapter: nekoweb({
-    apiKey: env.NEKOWEB_API_KEY,
-    // cookie: 'your nekoweb cookie for recently updated support (optional)',
-    folder: "dist",
-  }),
+  output: 'static'
 });
